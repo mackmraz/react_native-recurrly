ff8484ee727b81f5f425b796e5db2c44e5d105f5
Mack <mackmraz@gmail.com>
2026-04-28 19:09:49 -0400

    Remove temp buttons, add icon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,18 @@
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Image, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-7xl font-sans-extrabold text-primary">Home</Text>
-      <Link
-        href="/onboarding"
-        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-      >
-        Go to Onboarding
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-      >
-        Go to Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="mt-4 font-sans-bold rounded bg-primary text-white p-4"
-      >
-        Go to Sign Up
-      </Link>
+      <View className="home-header">
+        <View className="home-user">
+          <Image source={images.avatar} className="home-avatar" />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
